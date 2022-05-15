f09c12a37238a324d93d12f4241358c56bf87f8c
Antonio Zago <80283126+Antonio-Zago@users.noreply.github.com>
2022-05-15 19:33:51 -0300

    Validação com bean validation e exception handler

---
+++ src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
@@ -0,0 +1,51 @@
+package br.com.alura.forum.controller.form;
+
+import java.util.stream.Collectors;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import br.com.alura.forum.controller.dto.TopicoDTO;
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
+import br.com.alura.forum.repository.TopicoRepository;
+
+public class TopicoForm {
+	
+	@NotNull @NotEmpty
+	private String titulo; 
+	
+	@NotNull @NotEmpty
+	private String mensagem;
+	
+	@NotNull @NotEmpty
+	private String nomeCurso;
+	
+	
+	public Topico converter(CursoRepository CursoRepository) {
+		
+		Curso curso = CursoRepository.findByNome(nomeCurso);
+		
+		return new Topico(titulo, mensagem, curso);
+	}
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getMensagem() {
+		return mensagem;
+	}
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	public String getNomeCurso() {
+		return nomeCurso;
+	}
+	public void setNomeCurso(String nomeCurso) {
+		this.nomeCurso = nomeCurso;
+	}
+}
